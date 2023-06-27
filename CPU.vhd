cddaf3c6ae247d08b04651977acc2be7b5d6e3e6
YannisGitHubb <126159132+YannisGitHubb@users.noreply.github.com>
2023-06-27 23:28:34 +0200

    Update CPU.vhd

--- CPU.vhd
+++ CPU.vhd
@@ -3,7 +3,11 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity processor is
-
+generic (
+    N : integer := 32; -- default value is 32, but it can be changed
+    I : integer := 8;  -- default value is 8, but it can be changed
+    J : integer := 24  -- default value is 24, but it can be changed
+);
     port (clock, reset : in std_logic;
           Instruction : in std_logic_vector(N-1 downto 0);
           PSR_in : in std_logic_vector(N-1 downto 0);
@@ -15,7 +19,8 @@ end entity processor;
 
 architecture arch_processor of processor is
     -- Les signaux de contrôle pour l'instruction decoder
-    signal nPCsel, RegWr, UALSrc, PSREn, MemWr, WrSrc, RegSel: std_logic;
+    signal nPCsel, RegWr, ALUSrc, PSREn, MemWr, WrSrc, RegSel: std_logic;
+    signal ALUCtr :  std_logic_vector(1 downto 0);
     signal OP: std_logic_vector(1 downto 0);
     signal Rn, Rd, Rm: std_logic_vector(3 downto 0);
     signal Imm: std_logic_vector(I-1 downto 0);
@@ -26,25 +31,25 @@ architecture arch_processor of processor is
         -- Instruction Decoder
     inst_dec: entity work.decodeur 
         port map (
-                Instruction => Instruction,
-                PSR_out => PSR_out,
-                nPCsel => nPCsel,
-                RegWr => RegWr,
-                UALSrc => UALSrc,
-                PSREn => PSREn,
-                MemWr => MemWr,
-                WrSrc => WrSrc,
-                RegSel => RegSel,
-                OP => OP,
-                Rn => Rn,
-                Rd => Rd,
-                Rm => Rm,
-                Imm => Imm,
-                Offset => Offset
+                instruction => instruction,
+        PSR_in => PSR_in,
+        ALUCtr => ALUCtr,
+        ALUSrc => ALUSrc,
+        WrSrc => WrSrc,
+        nPCSel => nPCSel,
+        RegSel => RegSel,
+        PSREn => PSREn,
+        MemWr => MemWr,
+        RegWr => RegWr,
+        offset => offset,
+        Rn => Rn,
+        Rd => Rd,
+        Rm => Rm,
+        Imm => Imm
             );
 
         -- Unité de traitement (UAL)
-        ual: entity work.ual
+        ALU: entity work.alu
             port map (
                 OP => OP,
                 a => a,
@@ -53,7 +58,7 @@ architecture arch_processor of processor is
             );
 
         -- Unité de contrôle (control_unit)
-        ctrl_unit: entity work.control_unit
+        UC: entity work.UC
             port map (
                 DATAIN => DATAIN,
                 RST => reset,
